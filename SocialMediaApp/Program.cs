ee3759b2a1053201ed3672d0225841fc665bbfd9
darius006 <dariusconstantin098@gmail.com>
2025-12-09 23:32:35 +0200

    fixed partial login

--- SocialMediaApp/Program.cs
+++ SocialMediaApp/Program.cs
@@ -24,17 +24,17 @@ var app = builder.Build();
 //    SeedData.Initialize(services);
 //}
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseMigrationsEndPoint();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
